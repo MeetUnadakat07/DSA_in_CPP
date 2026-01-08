dd9b9862e94d96375a4da9a1ccdd5b77e39c6af8
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-01-09 00:29:35 +0530

    starting with recursion

---
+++ Recursion/check_is_array_sorted.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+bool isSorted(int arr[], int n, int i) {
+    if(i == n - 1) return true;
+    if(arr[i] > arr[i + 1]) return false;
+    return isSorted(arr, n, i + 1);
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    if(isSorted(arr, 5, 0)) {
+        cout << "The array is sorted." << endl;
+    } else {
+        cout << "The array is not sorted." << endl;
+    }
+}
